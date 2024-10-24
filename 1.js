1d9840cf83341fb42b15851a65e5b4c1a40885d0
shii128037 <shii128037@gmail.com>
2024-10-24 11:42:55 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,4 @@
+var age = 25;
+age = "eighteen"; // 錯誤：Type 'string' is not assignable to type 'number'.
+var Name = "Alice";
+Name = 123;
---
+++ 1.ts
@@ -0,0 +1,5 @@
+const age: number = 25;
+age = "eighteen"; // 錯誤：Type 'string' is not assignable to type 'number'.
+
+let Name: string = "Alice";
+Name = 123; //Type 'number' is not assignable to type 'string'.
